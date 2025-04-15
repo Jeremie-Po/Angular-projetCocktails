72edf9e334732dfd25746940a7bb85fc7081490d
Jérémie P <jpourageaux@hotmail.fr>
2025-04-15 11:29:24 +0200

    add header menu component

--- cocktails/src/app/components/header/header.component.ts
+++ cocktails/src/app/components/header/header.component.ts
@@ -1,28 +1,32 @@
 import {Component} from '@angular/core';
+import {HeaderMenuComponent} from './component/header-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [HeaderMenuComponent],
   template: `
-    <h3 class="flex-auto bold text-lg">Cocktails</h3>
-    <ul>
-      <li class="flex flex-row gap-16">
+    <div class="flex-auto text-bold text-lg">Cocktails</div>
+    <ul class="xs-hide flex flex-row gap-16">
+      <li>
         <a href="#">Liste des cocktails</a>
+      </li>
+      <li>
         <a href="#">Panier</a>
       </li>
     </ul>
+    <app-header-menu class=" hide xs-show  "/>
   `,
   styles: `
     :host {
+      position: relative;
       display: flex;
-      flex-direction: row;
       align-items: center;
       background-color: var(--primary);
       color: white;
       height: 56px;
       padding: 0 16px;
-    }`
+    }
+  `,
 })
 export class HeaderComponent {
-
 }
