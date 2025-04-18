211c937c410ed9f53e6aa389e170f20ac3cd9820
Jérémie P <jpourageaux@hotmail.fr>
2025-04-18 10:05:42 +0200

    add focus on searching bar when keydown

--- cocktails/src/app/components/cocktails/components/cocktail-filter.component.ts
+++ cocktails/src/app/components/cocktails/components/cocktail-filter.component.ts
@@ -1,4 +1,4 @@
-import {Component, model} from '@angular/core';
+import {Component, ElementRef, model, viewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 @Component({
@@ -7,10 +7,16 @@ import {FormsModule} from '@angular/forms';
     FormsModule
   ],
   template: `
-    <input class="input mb-20 w-full" type="text" placeholder="Rechercher un cocktail" [(ngModel)]="filter"/>
+    <input #input class="input mb-20 w-full" type="text" placeholder="Rechercher un cocktail" [(ngModel)]="filter"/>
   `,
   styles: ``
 })
 export class CocktailFilterComponent {
-  filter = model('')
+  filter = model('');
+
+  inputRef = viewChild<ElementRef<HTMLInputElement>>('input');
+
+  focus() {
+    this.inputRef()?.nativeElement.focus();
+  }
 }
--- cocktails/src/app/components/cocktails/components/cocktails-list.component.ts
+++ cocktails/src/app/components/cocktails/components/cocktails-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input, model, output, signal} from '@angular/core';
+import {Component, computed, ElementRef, input, model, output, signal, viewChild} from '@angular/core';
 import {Cocktail} from '../../../shared/interface';
 import {CocktailFilterComponent} from './cocktail-filter.component';
 
@@ -9,7 +9,7 @@ import {CocktailFilterComponent} from './cocktail-filter.component';
   ],
   template: `
     <h2 class="mb-20">Liste des cocktails</h2>
-    <app-cocktail-filter [(filter)]="filter"/>
+    <app-cocktail-filter #search [(filter)]="filter"/>
     <ul class="mb-20">
       @for (cocktail of filteredCocktails(); track cocktail._id) {
         @let active = cocktail._id === selectedCocktailId();
@@ -26,9 +26,14 @@ import {CocktailFilterComponent} from './cocktail-filter.component';
   styles: `li:hover {
     cursor: pointer;
     background-color: var(--light);
-  }`
+  }`,
+  host: {
+    '(window:keydown)': `keyboardInteraction($event)`, //editor/inspection unresolved javascript reference est passé à warning plutot qu'a error
+  },
 })
 export class CocktailsListComponent {
+  search = viewChild<CocktailFilterComponent>('search');
+
   cocktails = input<Cocktail[]>([])
   filter = signal<string>('');
 
@@ -37,4 +42,41 @@ export class CocktailsListComponent {
   })
 
   selectedCocktailId = model<string | null>(null);
+
+  keyboardInteraction({key}: KeyboardEvent) {
+    switch (key) {
+      case 'Escape' : {
+        this.selectedCocktailId.set(null);
+        break;
+      }
+      case 'Enter' : {
+        break;
+      }
+      case 'ArrowDown' : {
+        break;
+      }
+      case 'ArrowUp' : {
+        break;
+      }
+      default: {
+        this.search()?.focus();
+      }
+    }
+  }
+
+//explication du focus lors d'un kaydown sur la bar de recherche dans le composant enfant filter
+//
+// ✅ #input dans l’enfant : tu crées une référence locale sur l’élément <input> du template enfant → #input
+//
+// ✅ viewChild('input') dans le TS enfant : tu accèdes au vrai DOM via ElementRef, ce qui te permet de manipuler le champ (comme faire focus() dessus)
+//
+// ✅ Méthode .focus() dans l’enfant : encapsule la logique this.inputRef()?.nativeElement.focus() pour la rendre accessible de l’extérieur
+//
+// ✅ #search dans le parent : référence le composant enfant lui-même (pas le input, mais le composant CocktailFilterComponent)
+//
+// ✅ viewChild('search') dans le TS parent : accède à l’instance du composant enfant, donc à sa méthode .focus()
+//
+// ✅ Méthode keyboardInteraction() dans le parent : quand une touche est pressée, tu rediriges l’attention vers le champ de recherche via this.search()?.focus()
+
+
 }
