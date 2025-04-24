e0db16a04e77efc8e02e282970d514f2d3a82708
Jérémie P <jpourageaux@hotmail.fr>
2025-04-24 19:50:22 +0200

    add listing of cocktails in admin

--- cocktails/src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list.component.ts
+++ cocktails/src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {CocktailsService} from '../../../../../shared/services/cocktails.service';
 
 @Component({
   selector: 'app-admin-cocktails-list',
   imports: [],
   template: `
-    <p>
-      admin-cocktails-list works!
-    </p>
+    <h3 class="mb-20">Liste des cocktails</h3>
+    <ul>
+      @for (cocktail of cocktails(); track cocktail._id) {
+        <li class="flex gap-12 card mb-10 align-items-center">
+          <span class="flex-auto">{{ cocktail.name }}</span>
+          <button class="btn btn-primary">Editer</button>
+          <button (click)="deleteCocktail(cocktails.id)" class="btn btn-danger">Supprimer</button>
+        </li>
+      } @empty {
+        <p>Pas de cocktail pour le moment</p>
+      }
+    </ul>
   `,
-  styles: ``
+  host:
+    {class: "card"}
+  ,
+  styles: `
+    .card {
+      padding: 8px;
+    }`
 })
 export class AdminCocktailsListComponent {
+  cocktailsService = inject(CocktailsService);
 
+  cocktails = computed(() => this.cocktailsService.cocktailsResource.value() || []);
+
+  deleteCocktail(id: string) {
+    this.cocktailsService.deleteCocktail(id);
+  }
 }
