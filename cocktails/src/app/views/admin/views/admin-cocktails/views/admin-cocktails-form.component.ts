1880c0987b9b09118b9682b0a85a45cba9662176
Jérémie P <jpourageaux@hotmail.fr>
2025-04-25 09:58:26 +0200

    create angular form

--- cocktails/src/app/views/admin/views/admin-cocktails/views/admin-cocktails-form.component.ts
+++ cocktails/src/app/views/admin/views/admin-cocktails/views/admin-cocktails-form.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-admin-cocktails-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   template: `
-    <p>
-      admin-cocktails-form works!
-    </p>
+    <h3>Création d'un cocktail</h3>
+    <form class="w-half" [formGroup]="cocktailForm" (submit)="submit()">
+      <div class="flex flex-col mb-10">
+        <label for="name">Nom du cocktail</label>
+        <input formControlName="name" type="text" id="name">
+      </div>
+      <div class="flex flex-col mb-10">
+        <label for="description">Description du cocktail</label>
+        <input formControlName="description" type="text" id="description">
+      </div>
+      <div class="flex flex-col mb-10">
+        <label for="imageUrl">Image du cocktail</label>
+        <input formControlName="imageUrl" type="text" id="imageUrl">
+      </div>
+      <div formArrayName="ingredients" class="flex flex-col mb-20">
+        <label>Ingredients</label>
+        <button (click)=addIngredient() class="mb-10 btn btn-primary"> Ajouter un ingrédient</button>
+        @for (ingredient of ingredients.controls; track $index) {
+          <div class=" flex gap-16 mb-10">
+            <input class="flex-auto " type="text" [formControlName]="$index">
+            <!--            <button (click)="deleteHobby($index)" class="btn btn-danger">Delete</button>-->
+          </div>
+        }
+      </div>
+    </form>
   `,
-  styles: ``
+  host:
+    {class: "card"}
+  ,
+  styles: `
+    .card {
+      padding: 8px;
+    }`
 })
 export class AdminCocktailsFormComponent {
+  fb = inject(FormBuilder);
+  cocktailForm = this.fb.group({
+    name: [''],
+    description: [''],
+    imageUrl: [''],
+    ingredients: this.fb.array([]),
+  })
 
+  get ingredients() {
+    return this.cocktailForm.get('ingredients') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(new FormControl(''));
+  }
+
+  submit() {
+    console.log(this.cocktailForm.value);
+  }
 }
